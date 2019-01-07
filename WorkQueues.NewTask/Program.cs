0f97d99b59cb3342aaebfdac2eae72278e0446c5
James Chang <james.chang@xuenn.com>
2019-01-07 19:06:09 +0800

    new tutorial: worker queues

---
+++ WorkQueues.NewTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace WorkQueues.NewTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "rabbitmq.papajames.lab",
+                UserName = "admin",
+                Password = "pass.123"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "task_queue",
+                    basicProperties: properties,
+                    body: body);
+                Console.WriteLine("[x] Sent {0}", message);
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+        }
+    }
+}
